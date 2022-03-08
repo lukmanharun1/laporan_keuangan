5ee879a6fbd41a850e1e1a69ed6b79447a2ef343
Lukman Harun <nandes88.ni@gmail.com>
2022-03-08 21:45:41 +0700

    add seeder dividen

---
+++ constant/dividen-seeder.js
@@ -0,0 +1,7 @@
+const laporanKeuangan = require('./laporan_keuangan-seeder');
+module.exports = [
+  {
+    "id": laporanKeuangan[0].id,
+    "cash": 500
+  }
+]
--- constant/laporan_keuangan-seeder.js
+++ constant/laporan_keuangan-seeder.js
@@ -4,8 +4,8 @@ module.exports = [
     "id": "95e28076-d415-41c1-9e5c-d1449740018e",
     "emiten_id": emiten[0].id,
     "tanggal": "2022-03-31",
-    "jenis_laporan": "Q1",
+    "jenis_laporan": "TAHUNAN",
     "harga_saham": 11000,
-    "nama_file": "AALI Q1 31 MARET 2021.pdf"
+    "nama_file": "AALI TAHUNAN 31 DESEMBER 2021.pdf"
   }
 ]
