7c02a897774254a69eca8291a7d9a7698c779175
Lukman Harun <nandes88.ni@gmail.com>
2022-03-23 23:51:40 +0700

    memperbaiki validasi kode emiten

--- validation/validasi_kode_emiten.js
+++ validation/validasi_kode_emiten.js
@@ -1,9 +1,9 @@
 module.exports = (kodeEmiten) => {
-  const aplhabert = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+  if (kodeEmiten.length === 4) {
+    const aplhabert = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
                   'J','K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
                   'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
                 ];
-  if (kodeEmiten.length === 4) {
     for (const huruf of kodeEmiten.split('')) {
      if (!aplhabert.includes(huruf.toUpperCase())) {
        throw new Error('kode emiten is invalid');
