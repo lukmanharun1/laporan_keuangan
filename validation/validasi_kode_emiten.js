78c407de34c74bf5bcfc5731f80b92d9817b3af5
Lukman Harun <nandes88.ni@gmail.com>
2022-03-23 23:50:51 +0700

    memperbaiki validasi kode emiten tidak menggunakan regex

--- validation/validasi_kode_emiten.js
+++ validation/validasi_kode_emiten.js
@@ -1,7 +1,15 @@
 module.exports = (kodeEmiten) => {
-  var validasiHuruf = /^[a-zA-Z ]+$/;
-  if (kodeEmiten.match(validasiHuruf) && kodeEmiten.length === 4) {
-     return true;
-  } 
+  const aplhabert = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+                  'J','K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                  'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+                ];
+  if (kodeEmiten.length === 4) {
+    for (const huruf of kodeEmiten.split('')) {
+     if (!aplhabert.includes(huruf.toUpperCase())) {
+       throw new Error('kode emiten is invalid');
+     }
+    }
+    return true;
+  }
   throw new Error("kode emiten must be 4 letters");
 }
