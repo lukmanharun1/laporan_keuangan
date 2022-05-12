cd813d795fd40aa19f23699d318a6f3217ea4316
Lukman Harun <nandes88.ni@gmail.com>
2022-05-12 16:12:16 +0700

    mengurutkan (tanggal DESC) di laba rugi

--- controller/laba_rugi.js
+++ controller/laba_rugi.js
@@ -1,5 +1,10 @@
-const { LabaRugi, NeracaKeuangan, LaporanKeuangan, Emiten } = require('../models');
-const response = require('../helper/response');
+const {
+  LabaRugi,
+  NeracaKeuangan,
+  LaporanKeuangan,
+  Emiten,
+} = require("../models");
+const response = require("../helper/response");
 
 const find = async (req, res) => {
   try {
@@ -7,135 +12,178 @@ const find = async (req, res) => {
     // cari emiten untuk mendapatkan id
     const emiten = await Emiten.findOne({
       where: {
-        kode_emiten
+        kode_emiten,
       },
-      attributes: ['id', 'nama_emiten', 'jumlah_saham'],
+      attributes: ["id", "nama_emiten", "jumlah_saham"],
     });
     const { id: emiten_id, jumlah_saham, nama_emiten } = emiten;
     if (!emiten) {
-      return response(res, {
-        message: 'Emiten not found'
-      }, 404);
+      return response(
+        res,
+        {
+          message: "Emiten not found",
+        },
+        404
+      );
     }
     // cek jika jenis laporan Q4
-    if (jenis_laporan === 'Q4') {
+    if (jenis_laporan === "Q4") {
       const Q1 = [];
       const Q2 = [];
       const Q3 = [];
       const Q4 = [];
       const TAHUNAN = [];
-      
+
       // ambil seluruh laporan keuangan berdasarkan kode_emiten
       const findLaporanKeuangan = await LaporanKeuangan.findAll({
         where: {
-          emiten_id
+          emiten_id,
         },
-        attributes: ['tanggal', 'nama_file', 'jenis_laporan'],
-        order: [['tanggal', 'ASC']],
+        attributes: ["tanggal", "nama_file", "jenis_laporan"],
+        order: [["tanggal", "DESC"]],
         include: [
           {
             model: LabaRugi,
             attributes: [
-              'pendapatan', 'laba_kotor', 'laba_usaha',
-              'laba_sebelum_pajak', 'laba_bersih'
+              "pendapatan",
+              "laba_kotor",
+              "laba_usaha",
+              "laba_sebelum_pajak",
+              "laba_bersih",
             ],
-            as: 'laba_rugi'
+            as: "laba_rugi",
           },
           {
             model: NeracaKeuangan,
-            attributes: ['ekuitas', 'aset'],
-            as: 'neraca_keuangan'
-          }
+            attributes: ["ekuitas", "aset"],
+            as: "neraca_keuangan",
+          },
         ],
       });
       // filter laporan keuangan Q1, Q2, Q3, TAHUNAN
       findLaporanKeuangan.forEach((data) => {
-          if (data.jenis_laporan === 'Q1') {
-            Q1.push(data.laba_rugi);
-          } else if (data.jenis_laporan === 'Q2') {
-            Q2.push(data.laba_rugi);
-          } else if (data.jenis_laporan === 'Q3') {
-            Q3.push(data.laba_rugi);
-          } else if (data.jenis_laporan === 'TAHUNAN') {
-            TAHUNAN.push(data);
-          }
+        if (data.jenis_laporan === "Q1") {
+          Q1.push(data.laba_rugi);
+        } else if (data.jenis_laporan === "Q2") {
+          Q2.push(data.laba_rugi);
+        } else if (data.jenis_laporan === "Q3") {
+          Q3.push(data.laba_rugi);
+        } else if (data.jenis_laporan === "TAHUNAN") {
+          TAHUNAN.push(data);
+        }
       });
-      // susun laporan Q4 
+      // susun laporan Q4
       TAHUNAN.forEach((data, i) => {
         const { tanggal, nama_file } = data;
-        const { pendapatan, laba_kotor, laba_usaha, laba_sebelum_pajak, laba_bersih } = data.laba_rugi;
+        const {
+          pendapatan,
+          laba_kotor,
+          laba_usaha,
+          laba_sebelum_pajak,
+          laba_bersih,
+        } = data.laba_rugi;
         const { aset, ekuitas } = data.neraca_keuangan;
         Q4.push({
           tanggal,
           nama_file,
-          jenis_laporan: 'Q4',
+          jenis_laporan: "Q4",
           laba_rugi: {
-            pendapatan: pendapatan - Q3[i].pendapatan - Q2[i].pendapatan - Q1[i].pendapatan,
-            laba_kotor: laba_kotor - Q3[i].laba_kotor - Q2[i].laba_kotor - Q1[i].laba_kotor,
-            laba_usaha: laba_usaha - Q3[i].laba_usaha - Q2[i].laba_usaha - Q1[i].laba_usaha,
-            laba_sebelum_pajak: laba_sebelum_pajak - Q3[i].laba_sebelum_pajak - Q2[i].laba_sebelum_pajak - Q1[i].laba_sebelum_pajak,
-            laba_bersih: laba_bersih - Q3[i].laba_bersih - Q2[i].laba_bersih - Q1[i].laba_bersih,
+            pendapatan:
+              pendapatan -
+              Q3[i].pendapatan -
+              Q2[i].pendapatan -
+              Q1[i].pendapatan,
+            laba_kotor:
+              laba_kotor -
+              Q3[i].laba_kotor -
+              Q2[i].laba_kotor -
+              Q1[i].laba_kotor,
+            laba_usaha:
+              laba_usaha -
+              Q3[i].laba_usaha -
+              Q2[i].laba_usaha -
+              Q1[i].laba_usaha,
+            laba_sebelum_pajak:
+              laba_sebelum_pajak -
+              Q3[i].laba_sebelum_pajak -
+              Q2[i].laba_sebelum_pajak -
+              Q1[i].laba_sebelum_pajak,
+            laba_bersih:
+              laba_bersih -
+              Q3[i].laba_bersih -
+              Q2[i].laba_bersih -
+              Q1[i].laba_bersih,
           },
           neraca_keuangan: {
             aset,
-            ekuitas
-          }
+            ekuitas,
+          },
         });
       });
-      
+
       return response(res, {
-        status: 'success',
+        status: "success",
         jumlah_saham,
         nama_emiten,
-        data: Q4
+        data: Q4,
       });
-      
     }
     //  cari laporan keuangan berdasarkan kode_emiten, jenis_laporan
     const laporanKeuangan = await LaporanKeuangan.findAll({
       where: {
         emiten_id,
-        jenis_laporan
+        jenis_laporan,
       },
-      attributes: ['tanggal', 'nama_file'],
+      attributes: ["tanggal", "nama_file"],
+      order: [["tanggal", "DESC"]],
       include: [
         {
           model: LabaRugi,
           attributes: [
-            'pendapatan', 'laba_kotor', 'laba_usaha',
-            'laba_sebelum_pajak', 'laba_bersih'
+            "pendapatan",
+            "laba_kotor",
+            "laba_usaha",
+            "laba_sebelum_pajak",
+            "laba_bersih",
           ],
-          as: 'laba_rugi'
+          as: "laba_rugi",
         },
         {
           model: NeracaKeuangan,
-          attributes: ['ekuitas', 'aset'],
-          as: 'neraca_keuangan'
-        }
+          attributes: ["ekuitas", "aset"],
+          as: "neraca_keuangan",
+        },
       ],
     });
 
     if (!laporanKeuangan) {
-      return response(res, {
-        message: 'Laporan keuangan not found'
-      }, 404);
+      return response(
+        res,
+        {
+          message: "Laporan keuangan not found",
+        },
+        404
+      );
     }
 
     return response(res, {
-      status: 'success',
+      status: "success",
       jumlah_saham,
       nama_emiten,
-      data: laporanKeuangan
-    });  
-
+      data: laporanKeuangan,
+    });
   } catch (error) {
-    return response(res, {
-      status: 'error',
-      message: error.message
-    }, 500);
+    return response(
+      res,
+      {
+        status: "error",
+        message: error.message,
+      },
+      500
+    );
   }
-}
+};
 
 module.exports = {
-  find
-}
+  find,
+};
