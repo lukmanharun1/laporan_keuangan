710cb6fd5cc1abd69eaaee06ffe4a2bfd6086989
Lukman Harun <nandes88.ni@gmail.com>
2022-05-12 16:07:26 +0700

    mengurutkan (tanggal DESC) di neraca keuangan

--- controller/neraca_keuangan.js
+++ controller/neraca_keuangan.js
@@ -1,5 +1,5 @@
-const { NeracaKeuangan, Emiten, LaporanKeuangan } = require('../models');
-const response = require('../helper/response');
+const { NeracaKeuangan, Emiten, LaporanKeuangan } = require("../models");
+const response = require("../helper/response");
 
 const find = async (req, res) => {
   try {
@@ -7,55 +7,74 @@ const find = async (req, res) => {
     // cari emiten untuk mendapatkan jumlah_saham
     const emiten = await Emiten.findOne({
       where: {
-        kode_emiten
+        kode_emiten,
       },
-      attributes: ['id', 'nama_emiten', 'jumlah_saham'],
+      attributes: ["id", "nama_emiten", "jumlah_saham"],
     });
     const { id: emiten_id, nama_emiten, jumlah_saham } = emiten;
     if (!emiten) {
-      return response(res, {
-        message: 'Emiten not found'
-      }, 404);
+      return response(
+        res,
+        {
+          message: "Emiten not found",
+        },
+        404
+      );
     }
     //  cari laporan keuangan berdasarkan emiten_id, jenis_laporan
     const laporanKeuangan = await LaporanKeuangan.findAll({
       where: {
         emiten_id,
-        jenis_laporan
+        jenis_laporan,
       },
-      attributes: ['tanggal', 'harga_saham', 'nama_file'],
+      attributes: ["tanggal", "harga_saham", "nama_file"],
+      order: [["tanggal", "DESC"]],
       include: {
         model: NeracaKeuangan,
-        as: 'neraca_keuangan',
+        as: "neraca_keuangan",
         attributes: [
-        'aset', 'kas_dan_setara_kas', 'piutang', 'persediaan',
-        'aset_lancar', 'aset_tidak_lancar', 'liabilitas_jangka_pendek',
-        'liabilitas_jangka_panjang', 'liabilitas_berbunga', 'ekuitas'
-        ]
-      }
+          "aset",
+          "kas_dan_setara_kas",
+          "piutang",
+          "persediaan",
+          "aset_lancar",
+          "aset_tidak_lancar",
+          "liabilitas_jangka_pendek",
+          "liabilitas_jangka_panjang",
+          "liabilitas_berbunga",
+          "ekuitas",
+        ],
+      },
     });
 
     if (!laporanKeuangan) {
-      return response(res, {
-        message: 'Laporan keuangan not found'
-      }, 404);
+      return response(
+        res,
+        {
+          message: "Laporan keuangan not found",
+        },
+        404
+      );
     }
 
     return response(res, {
-      status: 'success',
+      status: "success",
       jumlah_saham,
       nama_emiten,
-      data: laporanKeuangan
-    });  
-
+      data: laporanKeuangan,
+    });
   } catch (error) {
-    return response(res, {
-      status: 'error',
-      message: error.message
-    }, 500);
+    return response(
+      res,
+      {
+        status: "error",
+        message: error.message,
+      },
+      500
+    );
   }
-}
+};
 
 module.exports = {
-  find
-}
+  find,
+};
