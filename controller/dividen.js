148e2766edf6ce0b58ec9c3a4c7244b030854688
Lukman Harun <nandes88.ni@gmail.com>
2022-05-12 16:14:49 +0700

    mengurutkan (tanggal DESC) di dividen

--- controller/dividen.js
+++ controller/dividen.js
@@ -1,52 +1,60 @@
-const { Dividen, LaporanKeuangan, Emiten } = require('../models');
-const response = require('../helper/response');
+const { Dividen, LaporanKeuangan, Emiten } = require("../models");
+const response = require("../helper/response");
 
 const find = async (req, res) => {
   try {
     const { kode_emiten } = req.params;
-     // cari emiten untuk mendapatkan id
-     const emiten = await Emiten.findOne({
+    // cari emiten untuk mendapatkan id
+    const emiten = await Emiten.findOne({
       where: {
-        kode_emiten
+        kode_emiten,
       },
-      attributes: ['id', 'nama_emiten', 'jumlah_saham'],
+      attributes: ["id", "nama_emiten", "jumlah_saham"],
     });
     const { id: emiten_id, jumlah_saham, nama_emiten } = emiten;
     //  cari laporan keuangan berdasarkan emiten_id, jenis_laporan TAHUNAN
     const laporanKeuangan = await LaporanKeuangan.findAll({
       where: {
         emiten_id,
-        jenis_laporan: 'TAHUNAN'
+        jenis_laporan: "TAHUNAN",
       },
-      attributes: ['tanggal', 'nama_file'],
+      attributes: ["tanggal", "nama_file"],
+      order: [["tanggal", "DESC"]],
       include: {
-          model: Dividen,
-          as: 'dividen',
-          attributes: ['cash'],
-        }
+        model: Dividen,
+        as: "dividen",
+        attributes: ["cash"],
+      },
     });
 
     if (!laporanKeuangan) {
-      return response(res, {
-        message: 'Laporan keuangan not found'
-      }, 404);
+      return response(
+        res,
+        {
+          message: "Laporan keuangan not found",
+        },
+        404
+      );
     }
 
     return response(res, {
-      status: 'success',
+      status: "success",
       jumlah_saham,
       nama_emiten,
-      data: laporanKeuangan
-    });  
-
+      data: laporanKeuangan,
+    });
   } catch (error) {
-    return response(res, {
-      status: 'error',
-      message: error.message
-    }, 500);
+    return response(
+      res,
+      {
+        status: "error",
+        message: error.message,
+      },
+      500
+    );
   }
-}
+};
 
 module.exports = {
-  find
-}
+  find,
+};
