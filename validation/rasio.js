55607455c81e9cb9f237f4239f8556dbba4539a8
Lukman Harun <nandes88.ni@gmail.com>
2022-05-16 13:34:47 +0700

    add validation rasio

---
+++ validation/rasio.js
@@ -0,0 +1,11 @@
+const { param } = require("express-validator");
+const validasiKodeEmiten = require("./validasi_kode_emiten");
+
+const find = () => [
+  param("kode_emiten").notEmpty().custom(validasiKodeEmiten),
+  param("jenis_laporan").notEmpty().isIn(["Q1", "Q2", "Q3", "Q4", "TAHUNAN"]),
+];
+
+module.exports = {
+  find,
+};
