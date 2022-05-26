4e623a298fe827453eea02f4eefdc51c1f711326
Lukman Harun <nandes88.ni@gmail.com>
2022-05-26 14:36:26 +0700

    memperbaiki models emiten dan migration nya

--- migrations/20220303065744-create-emiten-table.js
+++ migrations/20220303065744-create-emiten-table.js
@@ -1,28 +1,28 @@
-'use strict';
+"use strict";
 
-const constantColumn = require('../constant/constant-column');
+const constantColumn = require("../constant/constant-column");
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('emiten', {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("emiten", {
       jumlah_saham: {
         type: Sequelize.BIGINT,
-        allowNull: false
+        allowNull: false,
       },
       kode_emiten: {
         type: Sequelize.STRING(4),
         allowNull: false,
-        unique: true
+        unique: true,
       },
       nama_emiten: {
         type: Sequelize.STRING(255),
         allowNull: false,
-        unique: 'true'
+        unique: true,
       },
-      ...constantColumn(Sequelize)
-    })
+      ...constantColumn(Sequelize),
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('emiten');
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("emiten");
+  },
 };
--- models/emiten.js
+++ models/emiten.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Emiten extends Model {
     /**
@@ -12,40 +10,41 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       this.hasMany(models.LaporanKeuangan, {
-        foreignKey: 'emiten_id',
-        as: 'laporan_keuangan'
-      })
+        foreignKey: "emiten_id",
+        as: "laporan_keuangan",
+      });
     }
-  };
-  Emiten.init({
-    id: {
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false
+  }
+  Emiten.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      jumlah_saham: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+      },
+      kode_emiten: {
+        type: DataTypes.STRING(4),
+        allowNull: false,
+        unique: true,
+      },
+      nama_emiten: {
+        type: DataTypes.STRING(255),
+        unique: true,
+      },
     },
-    jumlah_saham: {
-      type: DataTypes.BIGINT,
-      allowNull: false
-    },
-    kode_emiten: {
-      type: DataTypes.STRING(4),
-      allowNull: false,
-      unique: true
-    },
-    nama_emiten: {
-      type: DataTypes.STRING(255),
-      primaryKey: false,
-      unique: true
-    },
-   
-  }, {
-    sequelize,
-    timestamps: true,
-    modelName: 'Emiten',
-    tableName: 'emiten',
-    underscored: true,
-    paranoid: true,
-  });
+    {
+      sequelize,
+      timestamps: true,
+      modelName: "Emiten",
+      tableName: "emiten",
+      underscored: true,
+      paranoid: true,
+    }
+  );
   return Emiten;
 };
