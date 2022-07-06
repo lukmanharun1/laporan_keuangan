d7d509f44abd4d6db1f9e005a5746681683a1ab7
Lukman Harun <nandes88.ni@gmail.com>
2022-07-06 14:47:22 +0700

    hitung potensiUpside INKP dan PTBA

--- laporan_keuangan_emiten/potensiUpside.js
+++ laporan_keuangan_emiten/potensiUpside.js
@@ -6,9 +6,20 @@ function potensiUpside(nilaiSaatIni, nilaiIntrinsik) {
   );
 }
 
-const nilaiSaatIni = 7500;
-const nilaiIntrinsik = 15000;
-const nilaiDownSide = 6000;
-console.log(`Capital Gain ${potensiUpside(nilaiSaatIni, nilaiIntrinsik)}`);
-console.log(`Margin Of Safety ${MOS(nilaiSaatIni, nilaiIntrinsik)}`);
-console.log(`Capital Loss ${potensiUpside(nilaiSaatIni, nilaiDownSide)}`);
+// INKP
+
+// const nilaiSaatIni = 7000;
+// const nilaiIntrinsik = 15000;
+// const nilaiDownSide = 6000;
+// console.log(`Capital Gain ${potensiUpside(nilaiSaatIni, nilaiIntrinsik)}`);
+// console.log(`Margin Of Safety ${MOS(nilaiSaatIni, nilaiIntrinsik)}`);
+// console.log(`Capital Loss ${potensiUpside(nilaiSaatIni, nilaiDownSide)}`);
+
+// PTBA
+
+// `const nilaiSaatIni = 3830;
+// const nilaiIntrinsik = 6000;
+// const nilaiDownSide = 2000;
+// console.log(`Capital Gain ${potensiUpside(nilaiSaatIni, nilaiIntrinsik)}`);
+// console.log(`Margin Of Safety ${MOS(nilaiSaatIni, nilaiIntrinsik)}`);
+// console.log(`Capital Loss ${potensiUpside(nilaiSaatIni, nilaiDownSide)}`);`
