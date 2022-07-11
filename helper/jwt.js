0590376579f3c5c6d94b23791b37d784489b8907
Lukman Harun <nandes88.ni@gmail.com>
2022-07-11 14:59:37 +0700

    ubah synchronous -> asynchronous jwt

--- helper/jwt.js
+++ helper/jwt.js
@@ -4,21 +4,50 @@ require("dotenv").config();
 const { ACTIVATION_TOKEN_SECRET, LOGIN_TOKEN_SECRET, FORGOT_PASSWORD_SECRET } =
   process.env;
 const createTokenActivation = (payload) =>
-  new Promise((resolve) =>
-    resolve(jwt.sign(payload, ACTIVATION_TOKEN_SECRET, { expiresIn: "15m" }))
+  new Promise((resolve, reject) =>
+    jwt.sign(
+      payload,
+      ACTIVATION_TOKEN_SECRET,
+      { expiresIn: "15m" },
+      (err, data) => {
+        if (err) reject(err);
+        resolve(data);
+      }
+    )
   );
 
 const verifyTokenActivation = (token) =>
-  new Promise((resolve) => resolve(jwt.verify(token, ACTIVATION_TOKEN_SECRET)));
+  new Promise((resolve, reject) =>
+    jwt.verify(token, ACTIVATION_TOKEN_SECRET, (err, decoded) => {
+      if (err) reject(err);
+      resolve(decoded);
+    })
+  );
 
 const createTokenLogin = (payload) =>
-  new Promise((resolve) =>
-    resolve(jwt.sign(payload, LOGIN_TOKEN_SECRET, { expiresIn: "7 days" }))
+  new Promise((resolve, reject) =>
+    jwt.sign(
+      payload,
+      LOGIN_TOKEN_SECRET,
+      { expiresIn: "7 days" },
+      (err, data) => {
+        if (err) reject(err);
+        resolve(data);
+      }
+    )
   );
 
 const createTokenForgotPassword = (payload) =>
-  new Promise((resolve) =>
-    resolve(jwt.sign(payload, FORGOT_PASSWORD_SECRET, { expiresIn: "15 m" }))
+  new Promise((resolve, reject) =>
+    jwt.sign(
+      payload,
+      FORGOT_PASSWORD_SECRET,
+      { expiresIn: "15 m" },
+      (err, data) => {
+        if (err) reject(err);
+        resolve(data);
+      }
+    )
   );
 
 module.exports = {
