09fbc79a457a12cd7d657db55bd17fb9064dae88
Lukman Harun <nandes88.ni@gmail.com>
2022-07-13 18:09:09 +0700

    menambahkan auth di routes arus-kas

--- routes/arus_kas.js
+++ routes/arus_kas.js
@@ -1,10 +1,17 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
-const controller = require('../controller/arus_kas');
-const validate = require('../middleware/validate');
-const validation = require('../validation/arus_kas');
+const controller = require("../controller/arus_kas");
+const validate = require("../middleware/validate");
+const { authentication } = require("../middleware/auth");
+const validation = require("../validation/arus_kas");
 
-router.get('/:kode_emiten/:jenis_laporan', validation.find(), validate, controller.find);
+router.get(
+  "/:kode_emiten/:jenis_laporan",
+  authentication,
+  validation.find(),
+  validate,
+  controller.find
+);
 
 module.exports = router;
