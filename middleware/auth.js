ab4535cc8b7b3a4e0d8cc50df68b6d88423798bb
Lukman Harun <nandes88.ni@gmail.com>
2022-07-13 17:57:59 +0700

    membuat middleware auth

---
+++ middleware/auth.js
@@ -0,0 +1,41 @@
+const response = require("../helper/response");
+const { verifyTokenLogin } = require("../helper/jwt");
+const authentication = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    if (!token) {
+      throw new Error("token tidak ditemukan!");
+    }
+    const decodeToken = await verifyTokenLogin(token);
+    req.decodeToken = decodeToken;
+    next();
+  } catch (error) {
+    return response(
+      res,
+      {
+        status: "error",
+        message: error.message,
+      },
+      400
+    );
+  }
+};
+
+const role = (req, res, next, roles = []) => {
+  if (!roles.includes(req.decodeToken.role)) {
+    return response(
+      res,
+      {
+        status: "error",
+        message: "Maaf anda tidak memiliki akses!",
+      },
+      403
+    );
+  }
+  next();
+};
+
+module.exports = {
+  authentication,
+  role,
+};
