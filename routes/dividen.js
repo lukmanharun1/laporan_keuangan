bdea58a6f92040d00729c8d34e33c26ceb8cfdb1
Lukman Harun <nandes88.ni@gmail.com>
2022-07-14 22:09:09 +0700

    menambahkan middleware auth di routes dividen

--- routes/dividen.js
+++ routes/dividen.js
@@ -1,10 +1,17 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
-const controller = require('../controller/dividen');
-const validate = require('../middleware/validate');
-const validation = require('../validation/dividen');
+const controller = require("../controller/dividen");
+const validate = require("../middleware/validate");
+const { authentication } = require("../middleware/auth");
+const validation = require("../validation/dividen");
 
-router.get('/:kode_emiten', validation.find(), validate, controller.find);
+router.get(
+  "/:kode_emiten",
+  authentication,
+  validation.find(),
+  validate,
+  controller.find
+);
 
 module.exports = router;
