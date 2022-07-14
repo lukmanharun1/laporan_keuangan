d5e093a5f743b836eb8593e45ddbdf42fc486d75
Lukman Harun <nandes88.ni@gmail.com>
2022-07-14 22:14:33 +0700

    menambahkan middleware auth di routes stock split

--- routes/stock_split.js
+++ routes/stock_split.js
@@ -3,8 +3,15 @@ const router = express.Router();
 
 const controller = require("../controller/stock_split");
 const validate = require("../middleware/validate");
+const { authentication } = require("../middleware/auth");
 const validation = require("../validation/stock_split");
 
-router.put("/:kode_emiten", validation.update(), validate, controller.update);
+router.put(
+  "/:kode_emiten",
+  authentication,
+  validation.update(),
+  validate,
+  controller.update
+);
 
 module.exports = router;
