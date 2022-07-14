e0c413676b125372bac84170adcf0b57ee52e0eb
Lukman Harun <nandes88.ni@gmail.com>
2022-07-14 22:07:53 +0700

    menambahkan middleware auth di routes emiten

--- routes/emiten.js
+++ routes/emiten.js
@@ -1,12 +1,35 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
-const controller = require('../controller/emiten');
-const validate = require('../middleware/validate');
-const validation = require('../validation/emiten');
+const controller = require("../controller/emiten");
+const validate = require("../middleware/validate");
+const { authentication, role } = require("../middleware/auth");
 
-router.get('/', validation.getAll(), validate, controller.getAll);
-router.post('/', validation.create(), validate, controller.create);
-router.put('/:id', validation.update(), validate, controller.update);
+const validation = require("../validation/emiten");
+
+router.get(
+  "/",
+  authentication,
+  validation.getAll(),
+  validate,
+  controller.getAll
+);
+router.post(
+  "/",
+  authentication,
+  role(["admin"]),
+  validation.create(),
+  validate,
+  controller.create
+);
+router.put(
+  "/:id",
+  authentication,
+  role(["admin"]),
+  validation.create(),
+  validation.update(),
+  validate,
+  controller.update
+);
 
 module.exports = router;
