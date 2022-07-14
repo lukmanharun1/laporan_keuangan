affe5196ec11dc470a7925bf2599e757e5ef4936
Lukman Harun <nandes88.ni@gmail.com>
2022-07-14 22:12:53 +0700

    menambahkan middleware auth di routes neraca keaungan

--- routes/neraca_keuangan.js
+++ routes/neraca_keuangan.js
@@ -1,10 +1,17 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
 
-const controller = require('../controller/neraca_keuangan');
-const validate = require('../middleware/validate');
-const validation = require('../validation/neraca_keuangan');
+const controller = require("../controller/neraca_keuangan");
+const validate = require("../middleware/validate");
+const { authentication } = require("../middleware/auth");
+const validation = require("../validation/neraca_keuangan");
 
-router.get('/:kode_emiten/:jenis_laporan', validation.find(), validate, controller.find);
+router.get(
+  "/:kode_emiten/:jenis_laporan",
+  authentication,
+  validation.find(),
+  validate,
+  controller.find
+);
 
 module.exports = router;
