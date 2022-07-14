d71a98583947cb3f0ddec95cd93e1e05a382dcb0
Lukman Harun <nandes88.ni@gmail.com>
2022-07-14 22:11:54 +0700

    menambahkan middleware auth di routes laporan keuangan

--- routes/laporan_keuangan.js
+++ routes/laporan_keuangan.js
@@ -6,21 +6,32 @@ const validate = require("../middleware/validate");
 const validation = require("../validation/laporan_keuangan");
 const uploadFile = require("../middleware/multer");
 const rewriteBody = require("../middleware/rewrite_body");
+const { authentication, role } = require("../middleware/auth");
 
 router.get(
   "/:kode_emiten/:tanggal",
+  authentication,
   validation.find(),
   validate,
   controller.find
 );
 router.post(
   "/",
+  authentication,
+  role(["admin"]),
   uploadFile,
   validation.create(),
   validate,
   rewriteBody,
   controller.create
 );
-router.delete("/:id", validation.destroy(), validate, controller.destroy);
+router.delete(
+  "/:id",
+  authentication,
+  role(["admin"]),
+  validation.destroy(),
+  validate,
+  controller.destroy
+);
 
 module.exports = router;
