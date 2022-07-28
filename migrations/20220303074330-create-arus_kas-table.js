424a9c59585e1ac456b83a4a7345f2adb6f7e003
Lukman Harun <nandes88.ni@gmail.com>
2022-07-28 22:42:08 +0700

    change (contant-column -> field) migration arus_kas

--- migrations/20220303074330-create-arus_kas-table.js
+++ migrations/20220303074330-create-arus_kas-table.js
@@ -1,25 +1,27 @@
-'use strict';
-const constantColumn = require('../constant/constant-column');
+"use strict";
+const field = require("../constant/field");
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('arus_kas', {
-      operasi: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      investasi: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      pendanaan: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      ...constantColumn(Sequelize)
-    })
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      "arus_kas",
+      field(Sequelize, {
+        operasi: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        investasi: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        pendanaan: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+      })
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('arus_kas');
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("arus_kas");
+  },
 };
