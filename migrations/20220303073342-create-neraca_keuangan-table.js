78834a8a5ef534e6a61384342240c04e83f4ca1d
Lukman Harun <nandes88.ni@gmail.com>
2022-07-28 22:41:35 +0700

    change (contant-column -> field) migration neraca_keuangan

--- migrations/20220303073342-create-neraca_keuangan-table.js
+++ migrations/20220303073342-create-neraca_keuangan-table.js
@@ -1,53 +1,55 @@
-'use strict';
-const constantColumn = require('../constant/constant-column');
+"use strict";
+const field = require("../constant/field");
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('neraca_keuangan', {
-      aset: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      kas_dan_setara_kas: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      piutang: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      persediaan: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      aset_lancar: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      aset_tidak_lancar: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      liabilitas_jangka_pendek: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      liabilitas_jangka_panjang: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      liabilitas_berbunga: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      ekuitas: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      ...constantColumn(Sequelize)
-    })
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      "neraca_keuangan",
+      field(Sequelize, {
+        aset: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        kas_dan_setara_kas: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        piutang: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        persediaan: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        aset_lancar: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        aset_tidak_lancar: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        liabilitas_jangka_pendek: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        liabilitas_jangka_panjang: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        liabilitas_berbunga: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        ekuitas: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+      })
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('neraca_keuangan');
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("neraca_keuangan");
+  },
 };
