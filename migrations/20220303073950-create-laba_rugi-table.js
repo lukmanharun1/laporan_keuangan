8156292d0b599c0b3d13ef4e6c3d495b4398bf27
Lukman Harun <nandes88.ni@gmail.com>
2022-07-28 22:41:54 +0700

    change (contant-column -> field) migration laba_rugi

--- migrations/20220303073950-create-laba_rugi-table.js
+++ migrations/20220303073950-create-laba_rugi-table.js
@@ -1,33 +1,35 @@
-'use strict';
-const constantColumn = require('../constant/constant-column');
+"use strict";
+const field = require("../constant/field");
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('laba_rugi', {
-      pendapatan: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      laba_kotor: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      laba_usaha: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      laba_sebelum_pajak: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      laba_bersih: {
-        type: Sequelize.BIGINT,
-        allowNull: false
-      },
-      ...constantColumn(Sequelize)
-    })
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      "laba_rugi",
+      field(Sequelize, {
+        pendapatan: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        laba_kotor: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        laba_usaha: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        laba_sebelum_pajak: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+        laba_bersih: {
+          type: Sequelize.BIGINT,
+          allowNull: false,
+        },
+      })
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('laba_rugi');
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("laba_rugi");
+  },
 };
