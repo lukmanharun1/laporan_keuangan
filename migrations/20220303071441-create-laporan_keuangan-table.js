f405d30e92b7a462e2965fedbcd26c8769887516
Lukman Harun <nandes88.ni@gmail.com>
2022-07-28 22:41:21 +0700

    change (contant-column -> field) migration laporan_keuangan

--- migrations/20220303071441-create-laporan_keuangan-table.js
+++ migrations/20220303071441-create-laporan_keuangan-table.js
@@ -1,33 +1,35 @@
-'use strict';
-const constantColumn = require('../constant/constant-column');
+"use strict";
+const field = require("../constant/field");
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('laporan_keuangan', {
-      emiten_id: {
-        type: Sequelize.UUID,
-        allowNull: false
-      },
-      tanggal: {
-        type: Sequelize.DATE,
-        allowNull: false
-      },
-      jenis_laporan: {
-        type: Sequelize.ENUM('Q1', 'Q2', 'Q3', 'TAHUNAN'),
-        allowNull: false
-      },
-      harga_saham: {
-        type: Sequelize.INTEGER(6),
-        allowNull: false,
-      },
-      nama_file: {
-        type: Sequelize.STRING(255),
-        allowNull: false
-      },
-      ...constantColumn(Sequelize)
-    })
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      "laporan_keuangan",
+      field(Sequelize, {
+        emiten_id: {
+          type: Sequelize.UUID,
+          allowNull: false,
+        },
+        tanggal: {
+          type: Sequelize.DATE,
+          allowNull: false,
+        },
+        jenis_laporan: {
+          type: Sequelize.ENUM("Q1", "Q2", "Q3", "TAHUNAN"),
+          allowNull: false,
+        },
+        harga_saham: {
+          type: Sequelize.INTEGER(6),
+          allowNull: false,
+        },
+        nama_file: {
+          type: Sequelize.STRING(255),
+          allowNull: false,
+        },
+      })
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('laporan_keuangan');
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("laporan_keuangan");
+  },
 };
