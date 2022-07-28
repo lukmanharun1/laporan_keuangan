36d11f51f696bb8f882f852925487a4d999ed890
Lukman Harun <nandes88.ni@gmail.com>
2022-07-28 22:42:25 +0700

    change (contant-column -> field) migration user

--- migrations/20220707073737-create-user-table.js
+++ migrations/20220707073737-create-user-table.js
@@ -1,28 +1,30 @@
 "use strict";
-const constantColumn = require("../constant/constant-column");
+const field = require("../constant/field");
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("user", {
-      nama_lengkap: {
-        type: Sequelize.STRING(128),
-        allowNull: false,
-      },
-      email: {
-        type: Sequelize.STRING(128),
-        primaryKey: true,
-        allowNull: false,
-      },
-      password: {
-        type: Sequelize.STRING(255),
-        allowNull: false,
-      },
-      role: {
-        type: Sequelize.ENUM("admin", "user"),
-        allowNull: false,
-      },
-      ...constantColumn(Sequelize),
-    });
+    await queryInterface.createTable(
+      "user",
+      field(Sequelize, {
+        nama_lengkap: {
+          type: Sequelize.STRING(128),
+          allowNull: false,
+        },
+        email: {
+          type: Sequelize.STRING(128),
+          primaryKey: true,
+          allowNull: false,
+        },
+        password: {
+          type: Sequelize.STRING(255),
+          allowNull: false,
+        },
+        role: {
+          type: Sequelize.ENUM("admin", "user"),
+          allowNull: false,
+        },
+      })
+    );
   },
 
   async down(queryInterface, Sequelize) {
